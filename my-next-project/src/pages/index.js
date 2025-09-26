e3d26f3758805a7c774c576f0c13cfa060e4687f
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2025-09-26 11:36:39 +0100

    feat: components

--- my-next-project/src/pages/index.js
+++ my-next-project/src/pages/index.js
@@ -1,24 +1,28 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import PrimeiroComponente from "@/components/PrimeiroComponente";
+import ComponenteComUmaProp from "@/components/ComponenteComUmaProp";
+import ComponenteComVariasProps from "@/components/ComponenteComVariasProps";
 
 export default function Home() {
   return (
     <div
-      className="min-h-screen bg-blue-500 flex flex-col items-center
+      className="min-h-screen bg-slate-400 flex flex-col items-center
 justify-center"
     >
       <h1 className="text-6xl font-bold text-white">Hello! </h1>{" "}
+      <PrimeiroComponente />
+      <ComponenteComUmaProp nome="Guilherme" />
+      <ComponenteComUmaProp nome="Arabella" />
+      <ComponenteComVariasProps
+        titulo="Perfil do Utilizador"
+        cor = "green"
+        nome="Maria Neto"
+        idade={25}
+
+
+        
+      />
     </div>
   );
 }
